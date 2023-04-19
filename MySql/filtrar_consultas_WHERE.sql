a4c41f8bc74d23708f9416eb41cbb605fefe988d
Marcelo Pereira <100322262+marcelopereiraB@users.noreply.github.com>
2023-04-19 09:17:57 -0300

    Update filtrar_consultas_WHERE.sql

--- MySql/filtrar_consultas_WHERE.sql
+++ MySql/filtrar_consultas_WHERE.sql
@@ -3,18 +3,18 @@ CREATE DATABASE estudos02;
 USE estudos02;
 
 CREATE TABLE tipos_produto(
-id INT NOT NULL AUTO_INCREMENT,
-descricao VARCHAR(60) NOT NULL,
-PRIMARY KEY (id)
+	id INT NOT NULL AUTO_INCREMENT,
+	descricao VARCHAR(60) NOT NULL,
+	PRIMARY KEY (id)
 );
  
 CREATE TABLE produtos(
-id INT NOT NULL AUTO_INCREMENT,
-descricao VARCHAR(60) NOT NULL,
-preco DECIMAL(8,2) NOT NULL,
-id_tipos_produto INT NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY(id_tipos_produto) REFERENCES tipos_produto(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	descricao VARCHAR(60) NOT NULL,
+	preco DECIMAL(8,2) NOT NULL,
+	id_tipos_produto INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY(id_tipos_produto) REFERENCES tipos_produto(id)
 );
 
 INSERT INTO tipos_produto (descricao) VALUES ('Computador');
